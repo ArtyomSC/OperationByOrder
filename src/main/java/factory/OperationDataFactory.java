1fb0c0b39c0a216ee5e75197e52d5f097b6ff9ec
ArtyomSC <@Anfiar>
2025-06-27 17:58:56 +0300

    fix Enum, DataProvider, Factory and KindList

--- src/main/java/factory/OperationDataFactory.java
+++ src/main/java/factory/OperationDataFactory.java
@@ -4,8 +4,6 @@ import enums.CurrencyKind;
 import to.OperationDataTO;
 
 public class OperationDataFactory {
-    //private String amount = "10";
-
     public OperationDataTO createOperationData(int amount, CurrencyKind currencyKind) {
         return new OperationDataTO(amount, currencyKind);
     }
